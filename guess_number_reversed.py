db54d8322a01ede67b167d76bce495503f248972
Kajetan7 <krmroske@gmail.com>
2023-03-25 15:05:32 +0100

    Second issue

--- guess_number_reversed.py
+++ guess_number_reversed.py
@@ -11,9 +11,28 @@ start_ = input()
 
 min_ = 0
 max_ = 1000
+option_ = ""
 
-guess = int((max_ - min_) / 2) + min_
-
-print(f'I pick: {guess}!')
-print('Is it the number you chose?')
-print('Pick one of following options: 1 - too big, 2 - too small, 3 - you are correct!')
+for i in range(11):
+    if option_ == "3":
+        print("\n Thanks for your time! It was a good game!")
+        break
+    if i == 10:
+        print("\n Don't cheat! You have switched the number! \n")
+        break
+    guess_ = int((max_ - min_) / 2) + min_
+    print(f'\n I pick: {guess_}!')
+    print('\n Is it the number you chose? \n')
+    while True:
+        option_ = input('Pick one of following options: 1 - too big, 2 - too small, 3 - you are correct!')
+        if option_ == "3":
+            print("\n Yeah! I won!")
+            break
+        elif option_ == "2":
+            min_ = guess_
+            break
+        elif option_ == "1":
+            max_ = guess_
+            break
+        else:
+            continue
